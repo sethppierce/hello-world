a7b7cc5671726cb3b75bffc2ecfd5dd065086360
Seth <sethparkerpierce@icloud.com>
2022-09-01 16:41:27 -0400

    functions added

--- app.js
+++ app.js
@@ -1,6 +1,69 @@
 'use strict';
 
-console.log('Hello World!');
+// console.log('Hello World!');
 
-let x= 11;
-console.log(x);
+// let x= 11;
+
+
+
+
+
+function greeting (){
+    let elitist = prompt('Are you a Fellow Food Elitist?');
+    if (elitist == 'yes'){
+        alert( 'Welcome Brethren!!')}
+        else if(elitist == 'Yes'){
+        alert( 'Welcome Brethren!!')}
+        else if (elitist == 'No'){
+        alert('YOU ARE NOT WELCOME HERE')}
+        else if(elitist == 'no'){
+        alert('YOU ARE NOT WELCOME HERE')}
+        else{}
+        
+        }
+
+
+
+        
+
+function showGreet(){document.write(message)}
+
+function eatThis(){
+    let answer = prompt('Would you eat this oil soaked mess?');
+    let message;
+    if (answer == 'yes'){
+        message = 'You disgust me.'
+        alert(message)}
+        else if(answer == 'Yes'){
+        message = 'You disgust me.';
+        alert(message)}
+        else if (answer == 'No'){
+        message = 'Thank you for being civilized!';
+        alert(message)}
+        else if(answer == 'no'){
+        message = 'Thank you for being civilized!';
+        alert(message)}
+        else{}
+    }
+
+ function eatThat(){
+     let answer = prompt('Would you eat this oil soaked mess?');
+     let message;
+     if (answer == 'yes'){
+            message = 'Congratulations, You chose the right answer.'
+            alert(message)}
+            else if(answer == 'Yes'){
+            message = 'Congratulations, You chose the right answer.';
+            alert(message)}
+            else if (answer == 'No'){
+            message = 'Get better opinions';
+            alert(message)}
+            else if(answer == 'no'){
+            message = 'Get better opinions';
+            alert(message)}
+            else{}
+        }
+
+function  alertSatire(){
+    alert ('THIS WEBSITE IS SATIRE!!')
+}
